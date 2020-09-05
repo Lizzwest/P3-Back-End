a606940543e37b8cbb50d59293e7960a39dfddb5
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-09-04 19:12:56 -0700

    [passport] Set up passport middleware, touch  passport.js to create token for authentication

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'You checking out my backend?! MY EYES ARE UP HERE!' });
 });
